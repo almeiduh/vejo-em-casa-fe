14e61b0cf7d44738eebe6ecadada51e55c7520a8
Ricardo Martins (ext.) <Ricardo.Martins@ctw.bmwgroup.com>
2020-03-30 23:25:50 +0100

    refactor mission component

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import Header from 'pages/Landing/Header';
 import Highlight from 'pages/Landing/Hightlight';
 
-import Mission from '../../components/LandingPageComponents/Mission';
 import ComoFunciona from './ComoFunciona';
+import Mission from './Mission';
 
 const LandingPage = () => {
   return (
