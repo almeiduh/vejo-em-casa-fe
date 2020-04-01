23d05bdfcf73cf5cd6c858bcedb9f9d549e8a026
diego.pereira <diego.pereira@farfetch.com>
2020-04-01 13:49:26 +0100

    fix: Use window.scrollTo to go to the sections because the header overlaps sections

--- src/pages/Landing/Header.jsx
+++ src/pages/Landing/Header.jsx
@@ -6,20 +6,27 @@ import { ReactComponent as PlaneIcon } from 'svg/veja_web_pag_landingpage_Icons-
 import styles from './header.module.css';
 
 const Header = () => {
+  const goToSection = section => {
+    // This is because the header overlaps the sections
+    window.scrollTo(0, document.getElementById(section).offsetTop - 80);
+    // To close after click
+    document.getElementById('menuBtn').checked = false;
+  };
+
   return (
     <header className={styles.header}>
       <a href="" className={styles.logo}><img alt="" width="148px" height="50px" src={logo} /></a>
       <input className={styles.menuBtn} type="checkbox" id="menuBtn" />
       <label className={styles.menuIcon} htmlFor="menuBtn"><span className={styles.navicon}></span></label>
       <ul className={styles.menu}>
-        <li><a href="#work">Sobre nós</a></li>
-        <li><a href="#about">Como funciona</a></li>
-        <li><a href="#careers">Missão</a></li>
-        <li><a href="#contact">Contactos</a></li>
-        <li><a href="#contact">Parcerias</a></li>
+        <li><a onClick={() => goToSection('sobrenos')}>Sobre nós</a></li>
+        <li><a onClick={() => goToSection('comofunciona')}>Como funciona</a></li>
+        <li><a onClick={() => goToSection('missao')}>Missão</a></li>
+        <li><a onClick={() => goToSection('contactos')}>Contactos</a></li>
+        <li><a onClick={() => goToSection('parcerias')}>Parcerias</a></li>
         <li>
           <PlaneIcon height="18px" width="28px" />
-          <a href="#poeteapar">Põe-te a par</a>
+          <a onClick={() => goToSection('poeteapar')}>Põe-te a par</a>
         </li>
       </ul>
     </header>
