aa1ac0674e941478bebfaacb896fbb321fb37623
diego.pereira <diego.pereira@farfetch.com>
2020-04-01 18:56:42 +0100

    fix: Make how it works section responsive

--- src/pages/Landing/HowItWorks.jsx
+++ src/pages/Landing/HowItWorks.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import icons from 'svg/veja_web_pag_landingpage_Icons-16.svg';
 
+import criadores from '../../svg/howItWorks/criadores.svg';
+import calendario from '../../svg/howItWorks/calendario.svg';
+import notificacoes from '../../svg/howItWorks/notificacoes.svg';
+import stream from '../../svg/howItWorks/stream.svg';
+import apoiar from '../../svg/howItWorks/apoiar.svg';
+
 import styles from './howitworks.module.css';
 
 const HowItWorks = () => {
@@ -30,6 +36,31 @@ const HowItWorks = () => {
           Achaste o conteúdo incrível? Apoia o criador através de MBWAY,
           PAYPAL...
         </p>
+      </div>
+
+      <div className={styles.smContainerMobile}>
+        <div className={styles.wrapper}>
+          <img alt="" className={styles.icon} src={criadores} />
+          <span className={styles.text}>Os criadores associam-se à Vejo Em Casa</span>
+        </div>
+        <div className={styles.wrapper}>
+          <img alt="" className={styles.icon} src={calendario} />
+          <span className={styles.text}>Definimos o calendário dos eventos sugeridos pelos criadores</span>
+        </div>
+        <div className={styles.wrapper}>
+          <img alt="" className={styles.icon} src={notificacoes} />
+          <div className={styles.text}>Tens uma agenda sempre atualizada onde podes subscrever os eventos que
+          mais te interessam</div>
+        </div>
+        <div className={styles.wrapper}>
+          <img alt="" className={styles.icon} src={stream} />
+          <div className={styles.text}>Show Time!</div>
+        </div>
+        <div className={styles.wrapper}>
+          <img alt="" className={styles.icon} src={apoiar} />
+          <span className={styles.text}>Achaste o conteúdo incrível? Apoia o criador através de MBWAY,
+          PAYPAL...</span>
+        </div>
       </div>
     </div>
   );
