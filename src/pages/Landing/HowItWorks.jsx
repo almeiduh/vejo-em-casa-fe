f641aee8a172fc8db96a894444c158589167cc2b
diego.pereira <diego.pereira@farfetch.com>
2020-04-02 11:40:09 +0100

    fix: Ajust icon size and text align on mobile

--- src/pages/Landing/HowItWorks.jsx
+++ src/pages/Landing/HowItWorks.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-// import icons from 'svg/veja_web_pag_landingpage_Icons-16.svg';
-
 import criadores from '../../svg/howItWorks/criadores.svg';
 import calendario from '../../svg/howItWorks/calendario.svg';
 import notificacoes from '../../svg/howItWorks/notificacoes.svg';
 import stream from '../../svg/howItWorks/stream.svg';
 import apoiar from '../../svg/howItWorks/apoiar.svg';
-import icons from 'svg/icons-and-text.svg';
+import icons from '../../svg/icons-and-text.svg';
 
 import styles from './howitworks.module.css';
 
@@ -27,26 +25,48 @@ const HowItWorks = () => {
 
       <div className={styles.smContainerMobile}>
         <div className={styles.wrapper}>
-          <img alt="" className={styles.icon} src={criadores} />
-          <span className={styles.text}>Os criadores associam-se à Vejo Em Casa</span>
-        </div>
-        <div className={styles.wrapper}>
-          <img alt="" className={styles.icon} src={calendario} />
-          <span className={styles.text}>Definimos o calendário dos eventos sugeridos pelos criadores</span>
-        </div>
-        <div className={styles.wrapper}>
-          <img alt="" className={styles.icon} src={notificacoes} />
-          <div className={styles.text}>Tens uma agenda sempre atualizada onde podes subscrever os eventos que
-          mais te interessam</div>
-        </div>
-        <div className={styles.wrapper}>
-          <img alt="" className={styles.icon} src={stream} />
-          <div className={styles.text}>Show Time!</div>
-        </div>
-        <div className={styles.wrapper}>
-          <img alt="" className={styles.icon} src={apoiar} />
-          <span className={styles.text}>Achaste o conteúdo incrível? Apoia o criador através de MBWAY,
+          <div className={styles.row}>
+            <div className={styles.icon}>
+              <img width="100" alt="" src={criadores} />
+            </div>
+            <div className={styles.text}>
+              <span>Os criadores associam-se à Vejo Em Casa</span>
+            </div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.icon}>
+              <img width="100" alt="" src={calendario} />
+            </div>
+            <div className={styles.text}>
+              <span>Definimos o calendário dos eventos sugeridos pelos criadores</span>
+            </div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.icon}>
+              <img width="100" alt="" src={notificacoes} />
+            </div>
+            <div className={styles.text}>
+              <span>Tens uma agenda sempre atualizada onde podes subscrever os eventos que
+          mais te interessam</span>
+            </div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.icon}>
+              <img width="100" alt="" src={stream} />
+            </div>
+            <div className={styles.text}>
+              <span>Show Time!</span>
+            </div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.icon}>
+              <img width="100" alt="" src={apoiar} />
+            </div>
+            <div className={styles.text}>
+              <span>Achaste o conteúdo incrível? Apoia o criador através de MBWAY,
           PAYPAL...</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
